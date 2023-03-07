41e21c2e10a6b51d2072946a51f6b1389ca50acb
Tatiana LLorente <tatianallorente@gmail.com>
2023-03-07 12:39:40 +0100

    Minor visual changes

--- src/theme/customTheme.js
+++ src/theme/customTheme.js
@@ -6,8 +6,10 @@ export const customTheme = (themeMode) => {
 
   const mainPrimaryColor = indigo[500]; //cyan[900]
   const mainSecondaryColor = themeMode === "dark" ? amber[400] : amber[500];
-  const textSecondaryColor = themeMode === "dark" ? indigo[300] : indigo[800];  
+  const textSecondaryColor = themeMode === "dark" ? indigo[300] : indigo[800];
   const backgroundColor = themeMode === "dark" ? '#303030' : '#fbfbfb';
+  const alertInfoBackgroundColor = themeMode === "dark" ? '#3e3e3e' : '#e1e1e1';
+  const alertInfoIconColor = themeMode === "dark" ? '#e1e1e1' : '#585858';
 
 
   const customTheme = createTheme({
@@ -19,6 +21,9 @@ export const customTheme = (themeMode) => {
       secondary: {
         main: mainSecondaryColor
       },
+      info: {
+        main: '#585858',
+      },
       text: {
         secondary: textSecondaryColor
       },
@@ -27,23 +32,35 @@ export const customTheme = (themeMode) => {
         paper: backgroundColor
       }
     },
-    typography: {  
+    typography: {
       h2: {
         fontFamily: 'Merienda, sans-serif'
-      },       
+      },
       h3: {
         fontFamily: 'Merienda, sans-serif'
-      },  
+      },
       h4: {
         fontFamily: 'Merienda, sans-serif'
-      }, 
+      },
       h5: {
         fontFamily: 'Merienda, sans-serif'
-      }, 
+      },
       h6: {
         fontFamily: 'Merienda, sans-serif'
       },
     },
+    components: {
+      MuiAlert: {
+        styleOverrides: {
+          standardInfo: {
+            backgroundColor: alertInfoBackgroundColor,
+            '& .MuiAlert-icon': {
+              color: alertInfoIconColor,
+            }
+          },
+        },
+      },
+    }
   });
 
   return customTheme;
