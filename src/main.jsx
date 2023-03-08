6c111e7ccf162deff61f05db8c3705e60ef723f4
Tatiana LLorente <tatianallorente@gmail.com>
2023-03-08 14:11:34 +0100

    Build with Vite

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './assets/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/MoviesApp-React">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
